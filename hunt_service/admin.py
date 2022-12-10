f0a9b68ef5200804e78ee6b2de17397daea630d6
Polina Koval <zagadka1316@gmail.com>
2022-12-10 17:33:13 +0300

    Add list_display in th admin models.

--- hunt_service/admin.py
+++ hunt_service/admin.py
@@ -5,14 +5,14 @@ from hunt_service.models import Applicant, Tag, Vacancy
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["name", "hf_id"]
 
 
 @admin.register(Applicant)
 class ApplicantAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["last_name", "first_name", "position", "hf_id"]
 
 
 @admin.register(Vacancy)
 class VacancyAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["position", "hf_id", "status"]
--- hunt_service/models.py
+++ hunt_service/models.py
@@ -39,6 +39,9 @@ class Applicant(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.position}"
 
+    def get_full_name(self):
+        return f"{self.last_name} {self.first_name} {self.middle_name}"
+
 
 class Vacancy(models.Model):
     class Statuses(models.TextChoices):
