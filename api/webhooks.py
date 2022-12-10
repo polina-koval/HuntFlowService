304553dc9ab47fe47b6f107679d001615b497d8b
Polina Koval <zagadka1316@gmail.com>
2022-12-10 17:44:51 +0300

    Move webhook endpoints to the separate file.

---
+++ api/webhooks.py
@@ -0,0 +1,56 @@
+import json
+
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from api.utils import (
+    add_applicant_to_vacancy,
+    create_or_update_applicant,
+    search_applicants,
+)
+from hunt_service.models import Tag, Vacancy
+
+
+class ApplicantWebHook(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        body = request.body
+        js = json.loads(body)
+        applicant = create_or_update_applicant(js["event"]["applicant"])
+        applicant.tags.clear()
+        if js["changes"] and js["changes"].get("applicant_tags", None):
+            tags = js["event"]["applicant_tags"]
+            for tag in tags:
+                tag_obj, _ = Tag.objects.get_or_create(
+                    name=tag["name"], hf_id=tag["id"]
+                )
+                applicant.tags.add(tag_obj)
+        return Response({}, status=status.HTTP_200_OK)
+
+
+class VacancyWebHook(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        body = request.body
+        js = json.loads(body)
+        vac_log = js["event"]["vacancy_log"]
+        if vac_log["state"]:
+            position = js["event"]["vacancy"]["position"]
+            vac_hf_id = js["event"]["vacancy"]["id"]
+            if vac_log["state"] == "OPEN":
+                vacancy, _ = Vacancy.objects.get_or_create(
+                    hf_id=vac_hf_id,
+                    defaults={
+                        "position": position,
+                        "status": Vacancy.Statuses.OPEN,
+                    },
+                )
+                applicants = search_applicants(position=position)
+                add_applicant_to_vacancy(applicants, vac_hf_id)
+            if vac_log["state"] == "REMOVED":
+                Vacancy.objects.filter(position=position).delete()
+        return Response({}, status=status.HTTP_200_OK)
